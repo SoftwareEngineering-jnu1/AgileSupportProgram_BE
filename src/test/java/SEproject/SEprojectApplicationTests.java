4eb196889d453d21cdfdf9efdcd1d9236a1c52ce
gmg010217 <gmg010217@gmail.com>
2024-10-11 11:21:32 +0900

    All File Initial commit

---
+++ src/test/java/SEproject/SEprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package SEproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SEprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
